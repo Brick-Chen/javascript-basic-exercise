a3a5fdd43c99297ae6340783068caa3d9dd89a04
YumingChen <mathcym@outlook.com>
2020-09-01 20:16:47 +0800

    [yuming chen] feat: finish array_to_list_spec.js

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,24 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined) {
+    throw new Error('Creating list from undefined array');
+  }
+
+  if (array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  const list = { value: -1, next: null };
+  let prev = list;
+  for (let i = 0; i < array.length; i += 1) {
+    const curr = { value: array[i], next: null };
+    prev.next = curr;
+    prev = curr;
+  }
+  return list.next;
 }
