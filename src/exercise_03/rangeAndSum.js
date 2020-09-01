0e51a792758f580a47f0c7d2fca91164d813ea0f
YumingChen <mathcym@outlook.com>
2020-09-01 19:52:58 +0800

    [yuming chen] feat: finish rangeAndSum.js.

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -7,7 +7,15 @@ export function range(start, end) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (start === end) {
+    return [];
+  }
+  const ans = [];
+  const step = start < end ? 1 : -1;
+  for (let i = start; i !== end; i += step) {
+    ans.push(i);
+  }
+  return ans;
 }
 
 export function sum(...numbers) {
@@ -17,6 +25,9 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let ans = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    ans += numbers[i];
+  }
+  return ans;
 }
