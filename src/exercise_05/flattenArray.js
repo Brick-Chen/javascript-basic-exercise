7e1db1b6babefeaac3e3a5a5c8b5b372c9a62a13
YumingChen <mathcym@outlook.com>
2020-09-01 21:02:21 +0800

    [yuming chen] feat: finish flatten_array_spec.js.

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,27 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+  if (array.length === 0) {
+    return [];
+  }
 
-  throw new Error('Please delete this line and implement the function');
+  const ans = [];
+  function helper(arr, level) {
+    if (level === 3) {
+      ans.push(arr);
+      return;
+    }
+    for (let i = 0; i < arr.length; i += 1) {
+      if (arr[i] instanceof Array) {
+        helper(arr[i], level + 1);
+      } else {
+        ans.push(arr[i]);
+      }
+    }
+  }
+  helper(array, 1);
+  return ans;
 }
